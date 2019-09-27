c4601ad2c0aef82c62d074fb622b305bc7a4ece7
streeter11 <streeter11@hotmail.com>
2019-09-27 22:31:51 +0100

    initial commit

---
+++ src/SearchBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SearchBox = () => {
+    return (
+        <div>
+            <input 
+            className= 'pa3 ba b--blue bg-lightest-blue'
+            type= 'search'
+            placeholder= 'Search Robots'
+            />
+        </div>
+    )
+}
+
+
+export default SearchBox;
