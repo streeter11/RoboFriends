81e6b6818fd80af0ae61b33e09c6aa9abcdc5192
streeter11 <streeter11@hotmail.com>
2019-10-02 21:55:09 +0100

    add App class

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import {robots} from './robots';
 import SearchBox from './SearchBox';
 
-const App = () => {
-    return (
-        <div className= "tc">
-            <h1>RoboFriends</h1>
-            <SearchBox />
-       
-            <CardList robots= { robots } />  
-        </div> 
-    )
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ""
+        }
+    }
+
+    onSearchChange(event) {
+        console.log(event);
+    }
+    render() {
+        return (
+            <div className= "tc">
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange= {this.onSearchChange}/>
+        
+                <CardList robots= { this.state.robots } />  
+            </div> 
+        )
+    }
+    
 }
 
-export default App;
+export default App; 
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({searchfield, searchChange}) => {
     return (
         <div>
             <input 
             className= 'pa3 ba b--blue bg-lightest-blue'
             type= 'search'
             placeholder= 'Search Robots'
+            onChange= {searchChange}
             />
         </div>
     )
