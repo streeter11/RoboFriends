e3c0fffce26154ca4f800a3b2429981b910e80ae
streeter11 <streeter11@hotmail.com>
2019-10-03 21:33:19 +0100

    update logic and add css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import {robots} from './robots';
 import SearchBox from './SearchBox';
+import './App.css';
 
 class App extends Component {
     constructor(){
@@ -12,16 +13,20 @@ class App extends Component {
         }
     }
 
-    onSearchChange(event) {
-        console.log(event);
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
     }
+
     render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });        
         return (
             <div className= "tc">
-                <h1>RoboFriends</h1>
+                <h1 className= "f1">RoboFriends</h1>
                 <SearchBox searchChange= {this.onSearchChange}/>
         
-                <CardList robots= { this.state.robots } />  
+                <CardList robots= { filteredRobots } />  
             </div> 
         )
     }
