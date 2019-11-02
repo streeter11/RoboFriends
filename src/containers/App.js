f072512b075a072e8776b24493b74e88e68c95dc
Harry Street <streeter11@hotmail.com>
2019-11-02 09:47:07 +0000

    refactor modules

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import {robots} from './robots';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
 import './App.css';
-import Scroll from './Scroll';
+import Scroll from '../components/Scroll';
 
 class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ""
         }
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(users => {this.setState({ robots: users})});
     }
 
     onSearchChange = (event) => {
@@ -19,10 +24,12 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state; 
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         });        
         return (
+            !robots.length ? <h1>Loading</h1> :
             <div className= "tc">
                 <h1 className= "f1">RoboFriends</h1>
                 <SearchBox searchChange= {this.onSearchChange}/>
