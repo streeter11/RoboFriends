35bd5786121bde986e2e00765501492776387dc4
Harry Street <streeter11@hotmail.com>
2019-11-08 20:28:11 +0000

    Updates

---
+++ precache-manifest.7139c03dd505476b38ce80f7e282d72a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e09040881e6f0cc7d10f80a66566461",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "d528a39b6f1b51bd21fc",
+    "url": "/Robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "83e6976c89b661fce694",
+    "url": "/Robofriends/static/css/main.9f1a522b.chunk.css"
+  },
+  {
+    "revision": "d528a39b6f1b51bd21fc",
+    "url": "/Robofriends/static/js/2.cfc72c35.chunk.js"
+  },
+  {
+    "revision": "83e6976c89b661fce694",
+    "url": "/Robofriends/static/js/main.604f182a.chunk.js"
+  },
+  {
+    "revision": "2bb2fac80baf4b00cb85",
+    "url": "/Robofriends/static/js/runtime~main.1cd4d11c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
